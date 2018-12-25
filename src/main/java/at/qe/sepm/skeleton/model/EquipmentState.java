3e49aec722a616b068579001f511206f8d6cfc02
Markus Hilpold <markus.hilpold@student.uibk.ac.at>
2018-12-25 21:01:14 +0100

    Upload New File

---
+++ src/main/java/at/qe/sepm/skeleton/model/EquipmentState.java
@@ -0,0 +1,16 @@
+package at.qe.sepm.skeleton.model;
+
+/**
+ * 
+ * @author markus
+ * Enumeration for available equipment states
+ */
+
+public class EquipmentState {
+	public enum Equipmentstate{
+		BOOKED,
+		AVAILABLE,
+		EXPIRED,
+		LOCKED
+	}
+}
