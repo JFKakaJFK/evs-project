6eacd3c3ad75d3281686f2cf198e9281b5c95bb2
ChrisKelter <Christopher.Kelter@student.uibk.ac.at>
2019-01-01 18:55:18 +0100

    add passwordEncoder

--- src/main/java/at/qe/sepm/skeleton/configs/WebSecurityConfig.java
+++ src/main/java/at/qe/sepm/skeleton/configs/WebSecurityConfig.java
@@ -2,11 +2,14 @@ package at.qe.sepm.skeleton.configs;
 
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
@@ -66,8 +69,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         //Configure roles and passwords via datasource
         auth.jdbcAuthentication().dataSource(dataSource)
                 .usersByUsernameQuery("select username, password, enabled from user where username=?")
-                .authoritiesByUsernameQuery("select user_username, roles from user_user_role where user_username=?");
-        // :TODO: use passwordEncoder and do not store passwords in plain text
+                .authoritiesByUsernameQuery("select user_username, roles from user_user_role where user_username=?")
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
     }
 
 }
